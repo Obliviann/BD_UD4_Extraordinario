7d68eceb44dcf6abe78a83b8329aa38f5d4af094
Olivia Sartorius Freschet <34093004+Obliviann@users.noreply.github.com>
2018-06-25 13:11:22 +0200

    Agregamos datos al doc. (crea la colleción implícitamente)

--- script.js
+++ script.js
@@ -13,3 +13,44 @@ firebase.initializeApp({
   
   // Initialize Cloud Firestore through Firebase
   var db = firebase.firestore();
+
+  //
+  db.collection("users").add({
+    first: "Ada",
+    last: "Lovelace",
+    born: 1815
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+
+// Add a second document with a generated ID.
+db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
+
+// Add a second document with a generated ID.
+db.collection("users").add({
+    first: "Alan",
+    middle: "Mathison",
+    last: "Turing",
+    born: 1912
+})
+.then(function(docRef) {
+    console.log("Document written with ID: ", docRef.id);
+})
+.catch(function(error) {
+    console.error("Error adding document: ", error);
+});
