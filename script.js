783a7ab3acc35558eafa2cfefa9864c24b91d3cc
Olivia Sartorius Freschet <34093004+Obliviann@users.noreply.github.com>
2018-06-25 13:06:24 +0200

    Added client dependencies and libraries + initialized cloud firestore

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// holi
+// Deberás solicitar de forma manual Firebase y Cloud Firestore:
+
+const firebase = require("firebase");
+// Required for side-effects
+require("firebase/firestore");
+
+//Inicializamos una instancia de Cloud Firestore
+firebase.initializeApp({
+    apiKey: '### FIREBASE API KEY ###',
+    authDomain: '### FIREBASE AUTH DOMAIN ###',
+    projectId: '### CLOUD FIRESTORE PROJECT ID ###'
+  });
+  
+  // Initialize Cloud Firestore through Firebase
+  var db = firebase.firestore();
